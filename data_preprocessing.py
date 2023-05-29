fb5f35d5f61590fc863a647dc78a97b9efb3aabe
banban555 <ban_ban_55@naver.com>
2023-05-30 03:18:03 +0900

    feat: 데이터 전처리 최적화

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,9 +6,12 @@ def remove_non_korean(x):
     x = re.sub('[^가-힣a-zA-Z0-9\s]', '', x)
     return None if x.strip() == '' else x
 
+
 def preprocess_data(prototypeData):
     hashtag_df = prototypeData[['record_id', 'tag_name']]
-    hashtag_df = hashtag_df.groupby('record_id')['tag_name'].apply(lambda x: ' '.join(x)).reset_index()
-    hashtag_df['tag_name'] = hashtag_df['tag_name'].apply(remove_non_korean)
+    hashtag_df = hashtag_df.groupby('record_id')['tag_name'].apply(
+        lambda x: ' '.join(x)).reset_index()
+    hashtag_df['tag_name'] = hashtag_df['tag_name'].str.replace(
+        "[^가-힣\s]", "", regex=True)
     hashtag_df = hashtag_df.dropna()
     return hashtag_df
